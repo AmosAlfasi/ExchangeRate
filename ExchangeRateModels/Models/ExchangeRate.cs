2ebf1f921731ae1072a947b9891df1171dcb0439
AmosAlfasi <amos13389@gmail.com>
2025-03-31 23:01:02 +0300

    Moved exchange rate models to a shared project for reuse
    - Extracted exchange rate models from RateFetcher to a separate project
    - Enabled usage of the models across multiple projects

--- ExchangeRateModels/Models/ExchangeRate.cs
+++ ExchangeRateModels/Models/ExchangeRate.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace RateFetcher.Models
+namespace ExchangeRateModels.Models
 {
     public class ExchangeRate
     {
--- RateFetcher/Services/RateFetcherWorker.cs
+++ RateFetcher/Services/RateFetcherWorker.cs
@@ -1,10 +1,5 @@
-﻿using RateFetcher.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ExchangeRateModels.Models;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace RateFetcher.Services
 {
