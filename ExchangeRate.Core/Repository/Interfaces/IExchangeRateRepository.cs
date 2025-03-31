6a802524c0093e359e92c4df598d203ff248a456
AmosAlfasi <amos13389@gmail.com>
2025-03-31 23:39:22 +0300

    change Modle Name

---
+++ ExchangeRate.Core/Repository/Interfaces/IExchangeRateRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Core.Repository.Interfaces
+{
+    public interface  IExchangeRateRepository
+    {
+        Task<List<ExchangeRateModel>> GetAllRatesAsync();
+    }
+}
