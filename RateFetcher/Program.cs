d6a3d2f386d054741ab95b3fb428d76c5f89fa0b
AmosAlfasi <amos13389@gmail.com>
2025-03-31 20:45:05 +0300

    Add project files.

---
+++ RateFetcher/Program.cs
@@ -0,0 +1,10 @@
+using RateFetcher;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
